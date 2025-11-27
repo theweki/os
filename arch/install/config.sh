4f1d9e93625b8ec791db06278f07294da84cf3cc
theweki <68937122+theweki@users.noreply.github.com>
2025-11-27 06:34:17 +0530

    Disable chaotic-aur setup in config.sh & added more essential packages
    Commented out the chaotic-aur setup section in config.sh to prevent execution during installation.

--- arch/install/config.sh
+++ arch/install/config.sh
@@ -19,16 +19,20 @@ pacman --noconfirm -S reflector
 reflector -c India -p https -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
 
-#chaotic-aur setup
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | tee -a /etc/pacman.conf
-pacman -Syy
+# chaotic-aur setup
+# pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+# pacman-key --lsign-key 3056513887B78AEB
+# pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+# pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+# echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | tee -a /etc/pacman.conf
+# pacman -Syy
 
 # Essential Packages
 pacman --noconfirm -S grub efibootmgr grub-btrfs wireplumber pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire bluez bluez-utils openssh thermald firewalld plocate
+pacman --noconfirm -S xorg xorg-xinit fwupd cups usbutils
+pacman --noconfirm -S mesa vulkan-intel intel-media-driver libva-utils libva-intel-driver # For Intel CPU
+pacman --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd
+
 
 # Locale, Keyboard
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
@@ -70,6 +74,8 @@ systemctl enable paccache.timer
 systemctl enable fstrim.timer
 systemctl enable reflector.timer
 systemctl enable plocate-updatedb.timer
+
+systemctl enable cups.socket
 
 # Corruption recovery -> btrfs-check
 sed -i 's/BINARIES=()/BINARIES=(btrfs)/g' /etc/mkinitcpio.conf
