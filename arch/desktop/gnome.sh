886fa856d0b5d38d48dfbf4b7cfb574ca4859cf3
theweki <68937122+theweki@users.noreply.github.com>
2025-11-27 06:35:20 +0530

    Streamline GNOME installation script
    Removed installation of some packages to streamline setup.

--- arch/desktop/gnome.sh
+++ arch/desktop/gnome.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-pacman --noconfirm -S xorg xorg-xinit fwupd gnome gnome-tweaks gnome-themes-extra power-profiles-daemon gnome-remote-desktop switcheroo-control cups usbutils
-pacman --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd
-pacman --noconfirm -S mesa vulkan-intel intel-media-driver libva-utils libva-intel-driver
-pacman --noconfirm -S yaru-gnome-shell-theme yaru-gtk-theme yaru-icon-theme extension-manager
+pacman --noconfirm -S gnome gnome-tweaks gnome-themes-extra power-profiles-daemon gnome-remote-desktop switcheroo-control
 
 sleep 1
 
 systemctl enable gdm.service
 systemctl enable switcheroo-control.service
 
-systemctl enable cups.socket
 systemctl enable avahi-daemon.socket
 
 sleep 2
